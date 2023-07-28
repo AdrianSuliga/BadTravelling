fa0b4c17f1cfd76188e0e0c953264946e63a8838
AdrianSuliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-07-29 00:55:54 +0200

    Update titlebar.cpp

--- titlebar.cpp
+++ titlebar.cpp
@@ -1,6 +1,7 @@
 #include "titlebar.h"
 #include "ui_titlebar.h"
 #include <QStyleOption>
+#include <QFontDatabase>
 #include <QPainter>
 
 TitleBar::TitleBar(QWidget *parent) :
@@ -8,6 +9,12 @@ TitleBar::TitleBar(QWidget *parent) :
     ui(new Ui::TitleBar)
 {
     ui->setupUi(this);
+
+    int id = QFontDatabase::addApplicationFont(":/other/other/Girassol-Regular.ttf");
+    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+    QFont Girassol(family);
+
+    ui->titleLabel->setFont(Girassol);
 }
 
 TitleBar::~TitleBar()
@@ -23,20 +30,7 @@ void TitleBar::paintEvent(QPaintEvent *event)
     style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
 }
 
-void TitleBar::on_minimiseButton_clicked()
-{
-    emit minimise();
-}
-
-
-void TitleBar::on_maximiseButton_clicked()
-{
-    emit maximise();
-}
-
-
-void TitleBar::on_exitButton_clicked()
-{
-    emit userWantsToExit();
-}
+void TitleBar::on_minimiseButton_clicked() {emit minimise();}
+void TitleBar::on_maximiseButton_clicked() {emit maximise();}
+void TitleBar::on_exitButton_clicked() {emit userWantsToExit();}
 
