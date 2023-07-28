e4d0e65de6e90991a91df34291038b89729ff356
AdrianSuliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-07-29 00:56:34 +0200

    Update titlescreen.cpp

--- titlescreen.cpp
+++ titlescreen.cpp
@@ -2,12 +2,28 @@
 #include "ui_titlescreen.h"
 #include <QStyleOption>
 #include <QPainter>
+#include <QFontDatabase>
 
 TitleScreen::TitleScreen(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::TitleScreen)
 {
     ui->setupUi(this);
+
+    int id = QFontDatabase::addApplicationFont(":/other/other/Girassol-Regular.ttf");
+    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+    QFont Girassol(family);
+
+    ui->titleLabel->setFont(Girassol);
+    ui->continueButton->setFont(Girassol);
+    ui->newgameButton->setFont(Girassol);
+    ui->aboutButton->setFont(Girassol);
+    ui->exitButton->setFont(Girassol);
+    ui->titleLabel->setStyleSheet("font-size: 80px;");
+    ui->continueButton->setStyleSheet("font-size: 40px;");
+    ui->newgameButton->setStyleSheet("font-size: 40px;");
+    ui->aboutButton->setStyleSheet("font-size: 40px;");
+    ui->exitButton->setStyleSheet("font-size: 40px;");
 }
 
 TitleScreen::~TitleScreen()
@@ -23,26 +39,8 @@ void TitleScreen::paintEvent(QPaintEvent *event)
     style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
 }
 
-void TitleScreen::on_exitButton_clicked()
-{
-    emit exitClicked();
-}
-
-
-void TitleScreen::on_aboutButton_clicked()
-{
-    emit aboutClicked();
-}
-
-
-void TitleScreen::on_newgameButton_clicked()
-{
-    emit newgameClicked();
-}
-
-
-void TitleScreen::on_continueButton_clicked()
-{
-    emit continueClicked();
-}
+void TitleScreen::on_exitButton_clicked() {emit exitClicked();}
+void TitleScreen::on_aboutButton_clicked() {emit aboutClicked();}
+void TitleScreen::on_newgameButton_clicked() {emit newgameClicked();}
+void TitleScreen::on_continueButton_clicked() {emit continueClicked();}
 
