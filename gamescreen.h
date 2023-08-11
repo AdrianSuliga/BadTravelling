52ce5532a5df98c0a49700c59213dd2012a0c663
AdrianSuliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-08-11 22:41:34 +0200

    Update gamescreen.h

--- gamescreen.h
+++ gamescreen.h
@@ -12,12 +12,30 @@ class GameScreen : public QWidget
     Q_OBJECT
 
 public:
-    explicit GameScreen(QWidget *parent = nullptr);
+    explicit GameScreen(QWidget *parent = nullptr, int gender = -1);
     ~GameScreen();
 protected:
     void paintEvent(QPaintEvent *event);
+private slots:
+    void loadVariables();
+    void on_continueButton_clicked();
+    //void enemyAttacked();
+    void userWantsToBuyWeapon();
+    void userWantsToBuyShield();
+    void userWantsToBuyHealth();
+    void userWantsToBuyBlahaj();
+    void userWantsToBuyManul();
+    void userWantsToBuyDrPieprzer();
+    //void userWantsToHeal();
+    void delay(int ms);
+
 private:
     Ui::GameScreen *ui;
+    int sex;
+    bool blahajOwned, manulOwned, drPieprzerOwned;
+    int heroAttack, heroDefense, heroHealth, heroMaxHealth, enemyAttack, enemyDefense, enemyHealth, enemyMaxHealth;
+    int wealth, weaponPrice, shieldPrice, healthPrice, blahajPrice, manulPrice, drPieprzerPrice, healingPrice;
+    short weaponLevel, shieldLevel, healthLevel, gameLevel;
 };
 
 #endif // GAMESCREEN_H
